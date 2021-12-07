46d99341a1547b7cf6e736a263ff5c820c89d523
Utsav Karan <ukaran11@gmail.com>
2021-12-07 19:30:43 +0530

    Utsav | Adding feathers express and socketio

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const feathers = require('@feathersjs/feathers')
-const app = feathers();
+const feathers = require('@feathersjs/feathers');
+const express = require('@feathersjs/express');
+const socketio = require('@feathersjs/socketio');
 
 class MessageService {
     constructor() {
         this.messages = []
     }
-
+    
     async find() {
         return this.messages;
     }
-
+    
     async create(data) {
         const message = {
             id: this.messages.length,
@@ -19,6 +20,15 @@ class MessageService {
         return message;
     }
 }
+
+const app = express(feathers());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(__dirname));
+
+app.configure(express.rest)
+app.configure(socketio)
 
 app.use('messages', new MessageService());
 
